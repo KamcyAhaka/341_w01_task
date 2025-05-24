f9377400e1e29871cf3a8a4b07198ac50530a720
Divine "KamCy" Ahaka <69519905+KamcyAhaka@users.noreply.github.com>
2025-05-24 23:06:26 +0100

    feat: W02 assignment completion

--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,66 @@ const getSingle = async (req, res) => {
   res.status(200).json(result);
 }
 
+const createContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favouriteColor: req.body.favouriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb.getDatabase().db('cse341').collection('users').insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user');
+  }
+};
+
+const updateContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
+
+  const contactId = new ObjectId(req.params.id);
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favouriteColor: req.body.favouriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
+    .getDatabase()
+    db('cse341').collection('users')
+    .replaceOne({ _id: contactId }, contact);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  // #swagger.tags=['Contacts']
+  const contactId = new ObjectId(req.params.id);
+
+  const response = await mongodb
+    .getDatabase()
+    db('cse341').collection('users')
+    .deleteOne({ _id: contactId }, true);
+
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user');
+  }
+};
+
 module.exports = {
-  getAll, getSingle
+  getAll, getSingle, createContact, updateContact, deleteContact 
 }
